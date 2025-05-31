46ccf3bb550b755f0fd86d2ff41ea4690a3eff58
xmd5a <zakaru@gmail.com>
2025-05-31 11:48:21 +0200

    implementation

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.BASE_URL || "/",
   plugins: [react(), tailwindcss()],
 });
